8c448bf10310b08c93c9f3d6366e609474b4364d
LeidyLozano <111013763+LeidyLozano@users.noreply.github.com>
2023-10-22 23:49:58 -0500

    Taller 1

--- main.ts
+++ main.ts
@@ -1,24 +1,49 @@
 import {Serie} from './serie.js';
-
-export const sr = new Serie("La casa de papel", "Netflix", 5 "owo", "https:");
+import { series } from './data.js';
 
 let serieTable: HTMLElement = document.getElementById("serie")!;
 let seriesTable: HTMLElement = document.getElementById("series")!;
-mostrarDatosSerie(sr);
+
 
 function mostrarDatosSerie(serie: Serie):void {
     let tbodySerie = document.createElement("tbody");
-    tbodySerie.innerHTML = `<tr><td colspan=2><img src="${serie.URLImagen}" height="100"></td></tr>
+    tbodySerie.innerHTML = `<tr><td>#</td><td>${serie.serieId}</td></tr>
     <tr><td>Nombres:</td><td>${serie.nombre}</td></tr>
     <tr><td>Canal:</td><td>${serie.canal}</td></tr>
-    <tr><td>Temporadas:</td><td>${serie.temporadas}</td></tr>
-    <tr><td>Temporadas:</td><td>${serie.descripcion}</td></tr>`
+    <tr><td>Temporadas:</td><td>${serie.temporadas}</td></tr>`
+
     serieTable.appendChild(tbodySerie);
 }
 
 function mostrarDatosSeries(series: Serie[]): void {
 
-    let seriesTbody: HTMLElement = document.createElement("tbody");
-    for 
-    
+    let numSeries:number=0;
+    let numTemporadas:number=0;
+    let promedio:number=0;
+
+    series.forEach(serie =>{
+
+        mostrarDatosSerie(serie);
+        numSeries++;
+        numTemporadas += serie.temporadas;
+    });
+
+    promedio = sacarPromedioSerie(numTemporadas, numSeries);
+
+    let averageSerie=document.createElement("tr");
+    averageSerie.innerHTML=`Seasons average: ${promedio}`
+    seriesTable.appendChild(averageSerie);
+
+
+}
+
+function sacarPromedioSerie(sumaTotal: number, numeroSeries: number): number{
+
+    let result:number=0;
+
+    result = sumaTotal/numeroSeries;
+
+    return result;
+
+
 }
--- serie.ts
+++ serie.ts
@@ -1,6 +1,6 @@
 export class Serie{
 
-    constructor(public nombre: string, public canal: string, public temporadas: number, public descripcion: string, public URLImagen: string){
+    constructor(public serieId: number, public nombre: string, public canal: string, public temporadas: number, public descripcion: string, public URLImagen: string){
 
     }
 }
