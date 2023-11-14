358a7dac303749387ab14771ded53ec87a84379c
LeidyLozano <111013763+LeidyLozano@users.noreply.github.com>
2023-11-14 00:25:28 -0500

    TypeScript es superior a a mí

--- main.ts
+++ main.ts
@@ -1,40 +1,19 @@
 import {Serie} from './serie.js';
 import { series } from './data.js';
 
-let serieTable: HTMLElement = document.getElementById("serie")!;
-let seriesTable: HTMLElement = document.getElementById("series")!;
+let seriesTable: HTMLElement = document.getElementById("Series")!;
 
 
-function mostrarDatosSerie(serie: Serie):void {
-    let tbodySerie = document.createElement("tbody");
-    tbodySerie.innerHTML = `<tr><td>#</td><td>${serie.serieId}</td></tr>
-    <tr><td>Nombres:</td><td>${serie.nombre}</td></tr>
-    <tr><td>Canal:</td><td>${serie.canal}</td></tr>
-    <tr><td>Temporadas:</td><td>${serie.temporadas}</td></tr>`
-
-    serieTable.appendChild(tbodySerie);
-}
-
-function mostrarDatosSeries(series: Serie[]): void {
-
-    let numSeries:number=0;
-    let numTemporadas:number=0;
-    let promedio:number=0;
-
-    series.forEach(serie =>{
-
-        mostrarDatosSerie(serie);
-        numSeries++;
-        numTemporadas += serie.temporadas;
+function mostrarDatosSerie(series: Serie[]):void {
+    series.forEach(c => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${c.serieId}</td> 
+                                <td data-id = ${c.serieId}><a>${c.nombre}</a></td>
+                                <td>${c.canal}</td>
+                                <td>${c.temporadas}</td>`;
+    
+        seriesTable.appendChild(trElement);
     });
-
-    promedio = sacarPromedioSerie(numTemporadas, numSeries);
-
-    let averageSerie=document.createElement("tr");
-    averageSerie.innerHTML=`Seasons average: ${promedio}`
-    seriesTable.appendChild(averageSerie);
-
-
 }
 
 function sacarPromedioSerie(sumaTotal: number, numeroSeries: number): number{
